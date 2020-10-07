cbe1c4f465e55539556ff4403088d98cbbc95fc3
Álvaro Fernández Rojas <noltari@gmail.com>
2020-10-07 11:31:59 +0200

    enigma2_pre_start.sh: remove unused variable
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- install/enigma2_pre_start.sh
+++ install/enigma2_pre_start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -O $UPDATE_FILE - https://raw.githubusercontent.com/Noltari/openspa-config/master/update.sh | bash
+wget -O - https://raw.githubusercontent.com/Noltari/openspa-config/master/update.sh | bash
 
 exit 0
