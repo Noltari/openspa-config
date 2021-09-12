8fb76264eef53f888c5c57d50e63d8e3c57a9b36
Álvaro Fernández Rojas <noltari@gmail.com>
2021-09-12 16:03:09 +0200

    files: Sorys 2021-09-11 update
    Signed-off-by: Álvaro Fernández Rojas <noltari@gmail.com>

--- cccam-channelinfo-convert.sh
+++ cccam-channelinfo-convert.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 FILES_DIR="$BASE_DIR/files"
@@ -11,25 +11,25 @@ cp /dev/null $BASE_DIR/oscam.srvid2
 echo "[Movistar]" > $BASE_DIR/oscam.services
 echo "caid = 1810" >> $BASE_DIR/oscam.services
 echo "provid =" >> $BASE_DIR/oscam.services
-echo -n "srvid =" >> $BASE_DIR/oscam.services
+echo -n "srvid = " >> $BASE_DIR/oscam.services
 
 cat $FILES_DIR/etc/CCcam.channelinfo | while read line; do
-	echo -n "P: " >> $FILES_DIR/etc/CCcam.prio
-	echo -n $line | sed 's/ .*//' >> $FILES_DIR/etc/CCcam.prio
-	echo -n "\t#" >> $FILES_DIR/etc/CCcam.prio
+	echo -ne "P: " >> $FILES_DIR/etc/CCcam.prio
+	echo -ne $line | sed 's/ .*//' >> $FILES_DIR/etc/CCcam.prio
+	echo -ne "\t#" >> $FILES_DIR/etc/CCcam.prio
 	echo $line | cut -d ' ' -f 2- >> $FILES_DIR/etc/CCcam.prio
 
-	echo -n "P: " >> $BASE_DIR/oscam.dvapi
-	echo -n $line | sed 's/ .*//' >> $BASE_DIR/oscam.dvapi
-	echo -n "\t#" >> $BASE_DIR/oscam.dvapi
+	echo -ne "P: " >> $BASE_DIR/oscam.dvapi
+	echo -ne $line | sed 's/ .*//' >> $BASE_DIR/oscam.dvapi
+	echo -ne "\t#" >> $BASE_DIR/oscam.dvapi
 	echo $line | cut -d ' ' -f 2- >> $BASE_DIR/oscam.dvapi
 
-	echo -n $line | sed 's/ .*//' | awk '{split($0,a,":"); printf "%s:%s", a[3], a[1]}' >> $BASE_DIR/oscam.srvid2
-	echo -n "@000000|" >> $BASE_DIR/oscam.srvid2
-	echo -n $line | cut -d ' ' -f 2- | tr -d $'\n' | tr -d '\"' >> $BASE_DIR/oscam.srvid2
+	echo -ne $line | sed 's/ .*//' | awk '{split($0,a,":"); printf "%s:%s", a[3], a[1]}' >> $BASE_DIR/oscam.srvid2
+	echo -ne "@000000|" >> $BASE_DIR/oscam.srvid2
+	echo -ne $line | cut -d ' ' -f 2- | tr -d $'\n' | tr -d '\"' >> $BASE_DIR/oscam.srvid2
 	echo "|||Movistar" >> $BASE_DIR/oscam.srvid2
 
-	echo -n $line | sed 's/ .*//' | awk '{split($0,a,":"); printf "%s,", a[3]}' >> $BASE_DIR/oscam.services
+	echo -ne $line | sed 's/ .*//' | awk '{split($0,a,":"); printf "%s,", a[3]}' >> $BASE_DIR/oscam.services
 done
 
 echo 0000 >> $BASE_DIR/oscam.services
